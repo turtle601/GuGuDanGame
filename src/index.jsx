8a42a775b27c4285881479272fe65aedb4b14f6c
turtle601 <turtle601@naver.com>
2021-10-18 19:02:28 +0900

    구구단 웹팩 빌드하기

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+const MulTable = require("./mulTable.jsx");
+
+ReactDom.render(<MulTable></MulTable>, document.querySelector('#root'));
